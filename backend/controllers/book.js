da86c781c7260f25fb0e4780417f4a61103b7e6b
Melissa <chopinetmelissa@live.fr>
2023-06-28 18:14:46 +0200

    debogage projet

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -8,10 +8,11 @@ exports.createBook = (req, res, next) => {
   const book = new Book({
     ...bookObject,
     userId: req.auth.userId,
-    imageUrl: `${req.protocol}://${req.get("host")}/images/${
-      req.file.filename
-    }`,
+    imageUrl: `${req.protocol}://${req.get("host")}/image/${req.file.filename}`,
+    averageRating: 0,
+    ratings: [],
   });
+  /*console.log(req.file);*/
 
   book
     .save()
@@ -87,15 +88,17 @@ exports.addRating = (req, res, next) => {
       .status(400)
       .json({ message: "La note doit être comprise entre 0 et 5." });
   }
+  /*console.log(req.params.id);
+  console.log(rating);*/
 
   Book.findOne({ _id: req.params.id })
     .then((book) => {
       if (!book) {
         return res.status(404).json({ message: "Livre non trouvé." });
       }
-
+      console.log("Toto");
       // Vérification si l'utilisateur a déjà noté ce livre
-      const existingRating = book.rating.find(
+      const existingRating = book.ratings.find(
         (r) => r.userId === req.auth.userId
       );
       if (existingRating) {
@@ -104,25 +107,23 @@ exports.addRating = (req, res, next) => {
           .json({ message: "L'utilisateur a déjà noté ce livre." });
       }
 
-      const filename = book.imageUrl.split("/images/")[1];
-      fs.unlink(`images/${filename}`, () => {
-        // Ajout de la note et mise à jour de la moyenne
-        const totalRating = book.rating.reduce((acc, r) => acc + r.rating, 0);
-        const newTotalRating = totalRating + rating;
-        const averageRating = newTotalRating / (book.rating.length + 1);
+      /*const filename = book.imageUrl.split("/images/")[1];
+      fs.unlink(`images/${filename}`, () => {*/
+      // Ajout de la note et mise à jour de la moyenne
+      const totalRating = book.ratings.reduce((acc, r) => acc + r.grade, 0);
+      const newTotalRating = totalRating + rating;
+      const averageRating = newTotalRating / (book.ratings.length + 1);
 
-        book.rating.push({ userId: req.auth.userId, rating });
-        book.averageRating = averageRating;
+      book.ratings.push({ userId: req.auth.userId, grade });
+      book.averageRating = averageRating;
 
-        book
-          .save()
-          .then((updatedBook) => {
-            res
-              .status(200)
-              .json({ message: "Rating ajoutée!", book: updatedBook });
-          })
-          .catch((error) => res.status(500).json({ error }));
-      });
+      book
+        .save()
+        .then((updatedBook) => {
+          res.status(200).json({ message: "Rating ajoutée!", updatedBook });
+        })
+        .catch((error) => res.status(500).json({ error }));
+      /*});*/
     })
     .catch((error) => res.status(500).json({ error }));
 };
@@ -131,8 +132,10 @@ exports.getBestBooks = (req, res, next) => {
   Book.find()
     .sort({ averageRating: -1 })
     .limit(3)
-    .then((books) => res.status(200).json(books))
-    .catch((error) => res.status(400).json({ error }));
+    .then((bestBooks) => res.status(200).json(bestBooks))
+    .catch((error) =>
+      res.status(500).json({ error: "Une erreur s'est produite" })
+    );
 };
 
 /* A CORRIGER*/
