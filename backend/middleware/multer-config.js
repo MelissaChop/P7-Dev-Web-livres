25439530764962dac7f5338ac95bf841f65afd55
Melissa <chopinetmelissa@live.fr>
2023-06-29 23:04:01 +0200

    fin journée 29 juin 2023

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,9 +1,12 @@
 const multer = require("multer");
+const sharp = require("sharp");
+const express = require("express");
 
 const MIME_TYPES = {
   "image/jpg": "jpg",
   "image/jpeg": "jpeg",
   "image/png": "png",
+  "image/webp": "webp",
 };
 
 const storage = multer.diskStorage({
@@ -17,6 +20,36 @@ const storage = multer.diskStorage({
   },
 });
 
-module.exports = multer({ storage: storage }).single("image");
+const upload = multer({ storage: storage }).single("image");
 
+const convertToWebP = (file) => {
+  sharp(file)
+    .webp()
+    .toFile("file.webp", function (err, info) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(info);
+      }
+    });
+};
+
+module.exports = (req, res, next) => {
+  upload(req, res, function (err) {
+    if (err) {
+      // Gérer les erreurs lors du téléchargement du fichier
+      return res
+        .status(400)
+        .json({ error: "An error occurred during upload." });
+    }
+
+    if (req.file) {
+      // Appeler la fonction de conversion si un fichier est présent
+      convertToWebP(req.file.path);
+    }
+
+    // Passer à l'étape suivante du middleware
+    next();
+  });
+};
 /*  A CORRIGER*/
