9c4e01441a07324bd52fae030d6f4eb6c511c78c
Melissa <chopinetmelissa@live.fr>
2023-07-02 14:56:11 +0200

    Changement ordre des routes book

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -7,11 +7,11 @@ const bookCtrl = require("../controllers/book");
 
 router.post("/", auth, multer, bookCtrl.createBook);
 router.post("/:id/rating", auth, bookCtrl.addRating);
-router.get("/", bookCtrl.getAllBook);
-router.get("/bestrating", bookCtrl.getBestBooks);
-router.get("/:id", bookCtrl.getOneBook);
 router.put("/:id", auth, multer, bookCtrl.modifyBook);
 router.delete("/:id", auth, bookCtrl.deleteBook);
+router.get("/bestrating", bookCtrl.getBestBooks);
+router.get("/:id", bookCtrl.getOneBook);
+router.get("/", bookCtrl.getAllBook);
 
 module.exports = router;
 
