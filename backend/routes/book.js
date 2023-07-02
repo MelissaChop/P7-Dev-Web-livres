dd0eca79c48c0f6b99b5ac4dbd973815f225eaff
Melissa <chopinetmelissa@live.fr>
2023-07-02 09:37:55 +0200

    Routes fonctionnent avec Postman

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -8,8 +8,8 @@ const bookCtrl = require("../controllers/book");
 router.post("/", auth, multer, bookCtrl.createBook);
 router.post("/:id/rating", auth, bookCtrl.addRating);
 router.get("/", bookCtrl.getAllBook);
-router.get("/:id", bookCtrl.getOneBook);
 router.get("/bestrating", bookCtrl.getBestBooks);
+router.get("/:id", bookCtrl.getOneBook);
 router.put("/:id", auth, multer, bookCtrl.modifyBook);
 router.delete("/:id", auth, bookCtrl.deleteBook);
 
