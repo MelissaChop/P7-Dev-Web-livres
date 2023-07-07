564777967db17c7a6e1b976d4b352e37db7ae59b
Melissa <chopinetmelissa@live.fr>
2023-07-07 09:55:50 +0200

    Derniere modification sur la suppression img que si req.file

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -32,18 +32,19 @@ exports.getOneBook = (req, res, next) => {
 
 exports.modifyBook = (req, res, next) => {
   /*FAIT*/
-  const bookObject = req.file
-    ? {
-        ...JSON.parse(req.body.book),
-        imageUrl: `${req.protocol}://${req.get("host")}/images/${
-          req.file.fileName
-        }`,
-      }
-    : { ...req.body };
 
-  delete bookObject._userId;
-  Book.findOne({ _id: req.params.id })
-    .then((book) => {
+  Book.findOne({ _id: req.params.id }).then((book) => {
+    const bookObject = req.file
+      ? {
+          ...JSON.parse(req.body.book),
+          imageUrl: `${req.protocol}://${req.get("host")}/images/${
+            req.file.fileName
+          }`,
+        }
+      : { ...req.body };
+    delete bookObject._userId;
+
+    if (req.file) {
       if (book.userId !== req.auth.userId) {
         res.status(401).json({ message: "Non autorisé!" });
       } else {
@@ -57,10 +58,19 @@ exports.modifyBook = (req, res, next) => {
             .catch((error) => res.status(500).json({ error }));
         });
       }
-    })
-    .catch((error) => {
-      res.status(400).json({ error });
-    });
+    } else {
+      if (book.userId !== req.auth.userId) {
+        res.status(401).json({ message: "Non autorisé!" });
+      } else {
+        Book.updateOne(
+          { _id: req.params.id },
+          { ...bookObject, _id: req.params.id }
+        )
+          .then(() => res.status(200).json({ message: "Livre modifié!" }))
+          .catch((error) => res.status(500).json({ error }));
+      }
+    }
+  });
 };
 
 exports.deleteBook = (req, res, next) => {
